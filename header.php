849bdfbe6f18420ffc47c5897b4e62d23d0014d6
Jan-Willem Visser <jw-1997@hotmail.com>
2018-10-10 14:56:23 +0200

    Github test.

---
+++ header.php
@@ -0,0 +1,6 @@
+    <div class="topnav">
+        <a href="#smartCaps">SmartCaps</a>
+        <a href="#aboutUs">Over ons</a>
+        <a href="#contact">Contact</a>
+    </div>  
+    <img class = "logo" src="images/logoSmartCaps.png">
