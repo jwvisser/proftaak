f83158071cfd8114ea2dc1daf3cf4aecf802f795
Jan-Willem Visser <jw-1997@hotmail.com>
2018-10-29 10:41:09 +0100

    Site SmartCaps.

--- aboutus.php
+++ aboutus.php
@@ -3,23 +3,36 @@
 <h1>Leer smartcaps kennen</h1>
 <h2>oprichters, achtergrond en geschiedenis</h2>
 
-<div class="column">
-	<div class="centerImage">
-		<img src="images/fotoRV.jpg" alt="Ren&eacute van Vliet" class="aboutPic">
-	</div>
-	<p>Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. </p>
+<div class="row">
+    <div class="column">
+        <div class="centerImage">
+            <img src="images/fotoRV.jpg" alt="Ren&eacute van Vliet" class="aboutPic">
+        </div>
+        <p>Ik ben Rene van Vliet. Ik woon in Breda en ben 35 jaar oud. Ik werk op de financiële afdeling van een verzekeringsmaatschappij. Daar houd ik mij vooral bezig met databeheer en programmeren. Hier werk ik nu 4 jaar. Om mijn kennis te verbeteren volg ik nu de opleiding Informatica. Samen met Stefan en Jan-Willem zijn we SmartCaps begonnen. Binnen SmartCaps houd ik mij bezig met het databeheer.</p>
+    </div>
+    <div class="column">
+        <div class="centerImage">
+            <img src="images/fotoSV.jpg" alt="Stefan Verloo" class="aboutPic">
+        </div>
+        <p>Mijn naam is Stefan Verloo. Ik ben 29 jaar oud, geboren en getogen in Zierikzee. Op het moment werk ik voor een IT bedrijf dat ERP- en mobiele software produceert voor andere bedrijven. Dit werk ben ik tegelijk begonnen met mijn opleiding Informatica. Samen met twee studiegenoten (Ren&eacute; en Jan-Willem) ben ik SmartCaps begonnen. Ik houd mij voornamelijk bezig met de administratie binnen SmartCaps.</p>
+    </div>
+    <div class="column">
+        <div class="centerImage">
+            <img src="images/fotoJW.jpg" alt="Jan-Willem Visser" class="aboutPic">
+        </div>
+        <p>Ik ben Jan-Willem Visser. Ik ben 21 jaar oud en kom uit Sleeuwijk. Naast mijn werkzaamheden bij SmartCaps werk ik nog bij een ICT bedrijf dat software ontwikkelt voor kinderdagverblijven. Hiervoor volg ik nu een opleiding Informatica, om mijn kennis uit te breiden op dit gebied. Mijn doel binnen SmartCaps is om de klant tevreden te stellen. Als je contact met ons opneemt, zal je vaak een reactie krijgen van mij.</p>
+    </div>
 </div>
-<div class="column">
-	<div class="centerImage">
-		<img src="images/fotoSV.jpg" alt="Stefan Verloo" class="aboutPic">
-	</div>
-	<p>Mijn naam is Stefan Verloo. Ik ben 29 jaar oud, geboren en getogen in Zierikzee. Op het moment werk ik voor een IT bedrijf dat ERP- en mobiele software produceert voor andere bedrijven. Dit werk ben ik tegelijk begonnen met mijn opleiding Informatica. Samen met twee studiegenoten (Ren&eacute; en Jan-Willem) ben ik SmartCaps begonnen. Ik houd mij voornamelijk bezig met de administratie binnen SmartCaps.</p>
+<hr>
+<div class="row">
+    <h2>Het bedrijf SmartCaps</h2>
+    <p>SmartCaps is een bedrijf dat is opgericht in 2018. Het begon allemaal met de gedachtegang dat het bijhouden van de hoeveelheid die je drinkt makkelijker moest zijn dan nu. Er waren wel applicaties waar je het zelf in kon vullen, maar nog geen tools die dit automatisch voor je deden. De SmartCap werd geboren en zo ook het bedrijf SmartCaps. Ondertussen zijn we een beginnend bedrijf met een volwaardig product. <br><br>
+    <div class="center">
+        Onze visie
+    </div>
+    <div class="italicised">"
+        Met de SmartCap is het bijhouden van wat je drinkt een koud kunstje. Wij zorgen ervoor dat jij 
+        vanaf nu altijd weet wat, hoeveel en wanneer je drinkt. Blijf voor altijd vitaal door het gebruik van de SmartCap en de app."
+    </div> </p>
 </div>
-<div class="column">
-	<div class="centerImage">
-		<img src="images/fotoJW.jpg" alt="Jan-Willem Visser" class="aboutPic">
-	</div>
-	<p>Ik ben Jan-Willem Visser. Ik ben 21 jaar oud en kom uit Sleeuwijk. Naast mijn werkzaamheden bij SmartCaps werk ik nog bij een ICT bedrijf dat software ontwikkelt voor kinderdagverblijven. Hiervoor volg ik nu een opleiding Informatica, om mijn kennis uit te breiden op dit gebied. Mijn doel binnen SmartCaps is om de klant tevreden te stellen. Als je contact met ons opneemt, zal je vaak een reactie krijgen van mij.</p>
-</div>
-
 <?php include("footer.php"); ?>
--- index.php
+++ index.php
@@ -1,32 +1,44 @@
 <?php include("header.php"); ?>
-		
-<div class="column">
-	<div class="centerImage">
-		<i class="fas fa-check-circle kwaliteit"></i> 
-	</div>
-	<div class="center">
-		<h1>Kwaliteit</h1>
-	</div>
-	<p>Wij vinden het belangrijk dat ons product van een goede kwaliteit is. Hieronder zien wij gebruiksvriendelijkheid, duurzaam materiaal en een goede prijs-kwaliteitverhouding. We zijn continu bezig met het verder ontwikkelen van de SmartCap. Daarom is de mening van u, de klant, erg belangrijk. Heb je een wens of idee voor de SmartCap, neem gerust contact op via de <a href="contact.php">contactpagina</a>! Wil je SmartCaps delen met vrienden via social media? Klik op de icoontjes onderaan de pagina.</p>
+<div class="row">		
+    <div class="column">
+        <div class="centerImage">
+            <i class="fas fa-check-circle kwaliteit"></i> 
+        </div>
+        <div class="center">
+            <h1>Kwaliteit</h1>
+        </div>
+        <p>Wij vinden het belangrijk dat ons product van een goede kwaliteit is. Hieronder zien wij gebruiksvriendelijkheid, duurzaam materiaal en een goede prijs-kwaliteitverhouding. We zijn continu bezig met het verder ontwikkelen van de SmartCap. Daarom is de mening van u, de klant, erg belangrijk. Heb je een wens of idee voor de SmartCap, neem gerust contact op via de <a href="contact.php">contactpagina</a>! Wil je SmartCaps delen met vrienden via social media? Klik op de icoontjes onderaan de pagina.</p>
+    </div>
+    <div class="column">
+        <div class="centerImage">
+            <i class="fab fa-pagelines milieu"></i> 
+        </div>
+        <div class="center">
+            <h1>Milieubewust</h1>
+        </div>
+        <p>Duurzaamheid speelt een grote rol binnen ons bedrijf. Drinkdopjes worden vaak gemaakt van plastic. Hier hebben wij een andere oplossing voor gevonden, namelijk: 3D printen! Door 3D printers is er minder behoefte aan transport van goederen. Er hoeven immers geen fysieke goederen over een grote afstand vervoerd te worden, maar alleen bouwtekeningen via het internet verstuurd te worden. Het uiteindelijke fysieke product wordt vervolgens lokaal door de 3D printer uitgeprint.</p>
+    </div>
+    <div class="column">
+        <div class="centerImage">
+            <i class="fas fa-mobile-alt mobiel"></i>
+        </div>
+        <div class="center">
+            <h1>Applicatie</h1>
+        </div>
+        <p> Via de applicatie op je mobiele telefoon kun je de opgeslagen gegevens van je SmartCap bijhouden. Deze gegevens kun je delen via social media, of vergelijken met je vrienden binnen de applicatie. De SmartCap applicatie is 100% gratis te downloaden in zowel de Appstore als de Google Playstore.
+        </p>
+    </div>
 </div>
-<div class="column">
-	<div class="centerImage">
-		<i class="fab fa-pagelines milieu"></i> 
-	</div>
-	<div class="center">
-		<h1>Milieubewust</h1>
-	</div>
-	<p>Duurzaamheid speelt een grote rol binnen ons bedrijf. Drinkdopjes worden vaak gemaakt van plastic. Hier hebben wij een andere oplossing voor gevonden, namelijk: 3D printen! Door 3D printers is er minder behoefte aan transport van goederen. Er hoeven immers geen fysieke goederen over een grote afstand vervoerd te worden, maar alleen bouwtekeningen via het internet verstuurd te worden. Het uiteindelijke fysieke product wordt vervolgens lokaal door de 3D printer uitgeprint.</p>
+<hr>
+<div class="row">		
+    <div class="centerImage">
+        <img src="images/logoSmartCaps.png" alt="SmartCaps"> 
+    </div>
+    <div class="center">
+        <h1>De SmartCap</h1>
+    </div>
+    <p>De SmartCap is de tool die jij nodig hebt, het is een universele dop en dat betekent dat deze op vrijwel alle flesjes past. De SmartCap zorgt ervoor dat je bij kunt houden hoeveel je drinkt, wat je drinkt maar ook wanneer je drinkt. Al deze gegevens worden doorgestuurd naar de SmartCaps database en vervolgens automatisch verwerkt. Hierdoor kan jij als gebruiker je statistieken van de SmartCap terug zien in de applicatie of op de website. Daarnaast kun je je opgeslagen gegevens vergelijken of delen met familie, vrienden, collega's of bijvoorbeeld leden van je sportvereniging. </p>
+    <p>De SmartCap is makkelijk te gebruiken, je draait hem op je flesje en klaar! Je kunt kiezen uit verschillende kleuren voor de Smartcap.
+De batterijduur is maar net afhankelijk van het gebruik. De SmartCap gaat minimaal 24 uur mee, waardoor je hem gemakkelijk 's nachts op kunt laden zodat je hem de volgende dag weer kunt gebruiken. De batterij is makkelijk op te laden met je telefoonoplader door de universele USB-poort.</p>
 </div>
-<div class="column">
-	<div class="centerImage">
-		<i class="fas fa-mobile-alt mobiel"></i>
-	</div>
-	<div class="center">
-		<h1>Applicatie</h1>
-	</div>
-	<p> Via de applicatie op je mobiele telefoon kun je de opgeslagen gegevens van je SmartCap bijhouden. Deze gegevens kun je delen via social media, of vergelijken met je vrienden binnen de applicatie. De SmartCap applicatie is 100% gratis te downloaden in zowel de Appstore als de Google Playstore.
-</p>
-</div>
-
 <?php include("footer.php"); ?>
