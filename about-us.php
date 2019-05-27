42dbdf163c44f7f19978ea19eaba912a94482876
Nynto <sverloo@avans.nl>
2019-05-27 14:58:38 +0200

    aboutColumn added

--- about-us.php
+++ about-us.php
@@ -13,25 +13,25 @@
 </style>
 
 <div class="row">
-    <div class="column">
+    <div class="aboutColumn">
         <div class="centerImage">
             <img src="assets/fotoRV.jpg" alt="Ren&eacute van Vliet" class="aboutPic">
         </div>
         <p>Ik ben Rene van Vliet. Ik woon in Breda en ben 35 jaar oud. Ik werk op de financiële afdeling van een verzekeringsmaatschappij. Daar houd ik mij vooral bezig met databeheer en programmeren. Hier werk ik nu 4 jaar. Om mijn kennis te verbeteren volg ik nu de opleiding Informatica. Samen met Stefan en Jan-Willem zijn we SmartCaps begonnen. Binnen SmartCaps houd ik mij bezig met het databeheer.</p>
     </div>
-    <div class="column">
+    <div class="aboutColumn">
         <div class="centerImage">
             <img src="assets/fotoSV.jpg" alt="Stefan Verloo" class="aboutPic">
         </div>
         <p>Mijn naam is Stefan Verloo. Ik ben 29 jaar oud, geboren en getogen in Zierikzee. Op het moment werk ik voor een IT bedrijf dat ERP- en mobiele software produceert voor andere bedrijven. Dit werk ben ik tegelijk begonnen met mijn opleiding Informatica. Samen met twee studiegenoten (Ren&eacute; en Jan-Willem) ben ik SmartCaps begonnen. Ik houd mij voornamelijk bezig met de administratie binnen SmartCaps.</p>
     </div>
-    <div class="column">
+    <div class="aboutColumn">
         <div class="centerImage">
             <img src="assets/fotoJW.jpg" alt="Jan-Willem Visser" class="aboutPic">
         </div>
         <p>Ik ben Jan-Willem Visser. Ik ben 21 jaar oud en kom uit Sleeuwijk. Naast mijn werkzaamheden bij SmartCaps werk ik nog bij een ICT bedrijf dat software ontwikkelt voor kinderdagverblijven. Hiervoor volg ik nu een opleiding Informatica, om mijn kennis uit te breiden op dit gebied. Mijn doel binnen SmartCaps is om de klant tevreden te stellen. Als je contact met ons opneemt, zal je vaak een reactie krijgen van mij.</p>
     </div>
-    <div class="column">
+    <div class="aboutColumn">
         <div class="centerImage">
             <img src="assets/fotoRDV.jpg" alt="Robbert de Vries" class="aboutPic">
         </div>
